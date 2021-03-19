8da4ca9746f3d13ff1291eaa2f54079f99c5387b
avadhesh.kumar <avadhesh.kumar.ka@partner.bmw.de>
2021-03-19 20:22:18 +0530

    initial commit Ecom Service

---
+++ src/main/java/com/api/EcomService/EcomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.EcomService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomServiceApplication.class, args);
+	}
+
+}
